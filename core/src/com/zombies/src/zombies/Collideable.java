aaa2a97be283fd7779d3f93fbb11243d7510dc5c
Coda <codabrink@gmail.com>
2015-02-03 14:12:23 -0500

    remove a bunch of extra files

--- core/src/com/zombies/src/zombies/Collideable.java
+++
@@ -1,9 +0,0 @@
-package com.zombies.src.zombies;
-
-import com.badlogic.gdx.physics.box2d.Fixture;
-
-public interface Collideable {
-
-	public void handleCollision(Fixture f);
-	
-}
--- core/src/com/zombies/src/zombies/Thumbpad.java
+++
@@ -1,7 +0,0 @@
-package com.zombies.src.zombies;
-
-/**
- * Created by Coda on 1/29/15.
- */
-public class Thumbpad {
-}
