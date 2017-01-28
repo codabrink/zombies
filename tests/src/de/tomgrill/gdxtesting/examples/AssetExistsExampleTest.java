31fc4aa1f5dc7ac583313cf484937b6075ed1187
Dakota <codabrink@gmail.com>
2017-01-28 12:15:57 -0500

    test box and room zoning

--- tests/src/de/tomgrill/gdxtesting/examples/AssetExistsExampleTest.java
+++ tests/src/de/tomgrill/gdxtesting/examples/AssetExistsExampleTest.java
@@ -10,7 +10,6 @@ import com.zombies.C;
 import com.zombies.GameView;
 import com.zombies.Zombies;
 import com.zombies.Zone;
-import com.zombies.map.MapGen;
 import com.zombies.map.room.Box;
 import com.zombies.map.room.Room;
 import com.zombies.map.thread.Generator;
@@ -27,10 +26,10 @@ public class AssetExistsExampleTest {
 	public void getAdjZone() {
 		Zombies instance = new Zombies();
 		instance.setScreen(new GameView());
+        GameView.gv.reset();
 
         Zone zone = Zone.getZone(0, 0);
         Room room = Generator.genRoom(new Vector2(300, 300));
-        Generator.zoneRoom(room);
 
         // assert a room is generating
 		assertTrue(zone.getRooms().size() > 0);
@@ -59,6 +58,18 @@ public class AssetExistsExampleTest {
 
     @Test
     public void testRoomZoning() {
+        GameView.gv.reset();
+
+        Zone z1 = Zone.getZone(0, 0);
+        Zone z2 = Zone.getZone(-1, 0);
+        Zone z3 = Zone.getZone(-1, -1);
+        Zone z4 = Zone.getZone(0, -1);
+
+        assertTrue(z1.getRooms().size() == 0);
+        assertTrue(z2.getRooms().size() == 0);
+        assertTrue(z3.getRooms().size() == 0);
+        assertTrue(z4.getRooms().size() == 0);
+
         HashMap<String, Box> boxMap = new HashMap<String, Box>();
         Box originBox = new Box(-C.BOX_RADIUS, -C.BOX_RADIUS);
         boxMap.put("0,0", originBox);
@@ -69,13 +80,34 @@ public class AssetExistsExampleTest {
         boxMap.put("0,-1", new Box(originBox.getPosition().cpy().add(0, -C.BOX_DIAMETER)));
 
         Room r = new Room(boxMap);
-        Generator.zoneRoom(r);
 
-        Zone z1 = Zone.getZone(0, 0);
-        Zone z2 = Zone.getZone(-1, 0);
-        Zone z3 = Zone.getZone(-1, -1);
-        Zone z4 = Zone.getZone(0, -1);
+        // zone 1
+        assertTrue(r.getZone() == z1);
+        assertTrue(z1.getRooms().size() == 1);
+        assertTrue(z1.getBoxes().size() == 3);
+        assertTrue(z1.getBoxes().contains(boxMap.get("0,0")));
+        assertTrue(z1.getBoxes().contains(boxMap.get("1,0")));
+        assertTrue(z1.getBoxes().contains(boxMap.get("0,1")));
 
-        System.out.println();
+        // zone 2
+        assertTrue(z2.getRooms().size() == 1);
+        assertTrue(z2.getBoxes().size() == 3);
+        assertTrue(z2.getBoxes().contains(boxMap.get("0,0")));
+        assertTrue(z2.getBoxes().contains(boxMap.get("-1,0")));
+        assertTrue(z2.getBoxes().contains(boxMap.get("0,1")));
+
+        // zone 3
+        assertTrue(z3.getRooms().size() == 1);
+        assertTrue(z3.getBoxes().size() == 3);
+        assertTrue(z3.getBoxes().contains(boxMap.get("0,0")));
+        assertTrue(z3.getBoxes().contains(boxMap.get("-1,0")));
+        assertTrue(z3.getBoxes().contains(boxMap.get("0,-1")));
+
+        // zone 4
+        assertTrue(z4.getRooms().size() == 1);
+        assertTrue(z4.getBoxes().size() == 3);
+        assertTrue(z4.getBoxes().contains(boxMap.get("0,0")));
+        assertTrue(z4.getBoxes().contains(boxMap.get("1,0")));
+        assertTrue(z4.getBoxes().contains(boxMap.get("0,-1")));
     }
 }
