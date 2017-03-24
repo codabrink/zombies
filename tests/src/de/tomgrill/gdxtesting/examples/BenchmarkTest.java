b9932d697fcac10699a0189fef55cf144f332bbc
Dakota <codabrink@gmail.com>
2017-03-23 20:25:58 -0400

    fix indents

--- tests/src/de/tomgrill/gdxtesting/examples/BenchmarkTest.java
+++ tests/src/de/tomgrill/gdxtesting/examples/BenchmarkTest.java
@@ -19,25 +19,24 @@ package de.tomgrill.gdxtesting.examples;
 import com.badlogic.gdx.math.Vector2;
 import com.zombies.abstract_classes.Overlappable;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class BenchmarkTest {
 
-	@Test
-	public void BenchmarkOverlappable() {
-		Overlappable o1 = new Overlappable(new Vector2(0, 0), 100, 100);
-		Overlappable o2 = new Overlappable(new Vector2(50, 50), 100, 100);
+    @Test
+    public void BenchmarkOverlappable() {
+        Overlappable o1 = new Overlappable(new Vector2(0, 0), 100, 100);
+        Overlappable o2 = new Overlappable(new Vector2(50, 50), 100, 100);
 
-		assertTrue(o1.overlaps(o2));
+        assertTrue(o1.overlaps(o2));
 
         long start = System.currentTimeMillis(), end;
-		for (int i = 0; i < 1000000; i++) {
-			o1.overlaps(o2);
-		}
-		end = System.currentTimeMillis();
-		System.out.println("A million square overlap tests took " + (System.currentTimeMillis() - start) + " milliseconds.");
-	}
+        for (int i = 0; i < 1000000; i++) {
+            o1.overlaps(o2);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("A million square overlap tests took " + (System.currentTimeMillis() - start) + " milliseconds.");
+    }
 }
